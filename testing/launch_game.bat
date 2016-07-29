dd491942d502b2d9218cfe4dc98be371b600a2b0
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2016-07-30 01:56:30 +0200

    Initial commit

---
+++ testing/launch_game.bat
@@ -0,0 +1,11 @@
+@echo off
+set sessionid=58751
+set gamename=PlatformerGame
+
+set runner="C:\Users\Tom Dobbelaere\AppData\Roaming\GameMaker-Studio\Runner.exe" -game
+set target=C:\Users\Tom Dobbelaere\AppData\Local\gm_ttt_%sessionid%\
+echo %target%
+cd /D %target%
+FOR /F "delims=" %%i IN ('dir gm* /b /ad-h /t:c /od') DO SET a=%%i
+%runner% %a%\%gamename%.win
+exit
